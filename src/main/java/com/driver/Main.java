6acd4538cbde63326048e355cc9faa05edd37e17
ShaikhAnis30 <109184307+ShaikhAnis30@users.noreply.github.com>
2023-01-11 14:04:00 +0530

    Polymorphism Assignment Completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,31 @@
 package com.driver;
 
 public class Main {
-
+  class Product{
+    public int product(int x, int y){
+      int ans = x * y;
+      return ans;
+    }
+    
+    @Overload
+    public int product(int x, int y, int z){
+      int ans = x * y * z;
+      return ans;
+    }
+    
+    @Overload
+    public double product(double x, double y){
+      double ans = x * y;
+      return ans;
+    }
+    
+  }
+  public static void main(String args[]) {
+    Product p = new Product();
+    p.product(2, 3);
+    
+    p.product(4, 5, 8);
+    
+    p.product(7.48, 5.65);
+  }
 }
