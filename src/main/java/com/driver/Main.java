edd587eee96219c5ce2d5a11cb5b1dbd7371f0b6
ShaikhAnis30 <anisshaikh8535@gmail.com>
2023-01-13 10:46:01 +0530

    Update 3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,33 +3,34 @@ package com.driver;
 import java.util.Scanner;
 
 public class Main {
-  public class Product{
-    public int product(int x, int y){
-      int ans = x * y;
-      return ans;
-    }
-    
+  public static class Product{       // task 1
 
-    public int product(int x, int y, int z){
-      int ans = x * y * z;
-      return ans;
+    public int product(int x, int y){   // task 3
+      return x * y;
     }
-    
 
-    public double product(double x, double y){
-      double ans = x * y;
-      return ans;
+    public int product(int x, int y, int z) {   // task 4
+      return x * y * z;
+    }
+
+    public double product(double x, double y) {  // task 5
+      return x * y;
     }
     
   }
-  public void main(String args[]) {
+  public static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
 
-    Product p = new Product();
-    p.product(sc.nextInt(), sc.nextInt());
-    
-    p.product(sc.nextInt(), sc.nextInt(), sc.nextInt());
-    
-    p.product(sc.nextDouble(), sc.nextDouble());
+    Product p = new Product();    // task 2
+
+    int ans1 = p.product(sc.nextInt(), sc.nextInt());  // task 3 called
+//    System.out.println(ans1);
+
+    int ans2 = p.product(sc.nextInt(), sc.nextInt(), sc.nextInt());  // task 4 called
+//    System.out.println(ans2);
+
+    double ans3 = p.product(sc.nextDouble(), sc.nextDouble());  // task 5 called
+//    System.out.println(ans3);
+
   }
 }
